fdf814a74473dd26f66538021e90c5346f42cdaf
Adrien Vidal <adriensg.vidal@gmail.com>
2020-04-16 14:32:43 +0200

    init social

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 import Nav from "./Nav/Nav";
+import Social from "./Social/Social";
 
 function Sidebar() {
-    return (
-        <div className="sidebar">
-        <Nav />
-      </div>
-    )
+  return (
+    <div className="sidebar">
+      <Nav />
+      <Social />
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
---
+++ src/components/Sidebar/Social/Social.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+// react fontAwesome
+import {
+  faHome,
+  faUser,
+  faLaptopCode,
+  faEnvelope
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+class Social extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // hasError: false
+    };
+  }
+
+  render() {
+    return (
+      <div>Social</div>
+    );
+  }
+}
+
+export default Social;
